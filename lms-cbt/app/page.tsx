27ad0ca69d1be5c0c6a83e60e57c8f994b9d3ee9
SangHui48 <vodlrm123@naver.com>
2023-12-25 23:29:04 +0900

    14m 21s

--- lms-cbt/app/page.tsx
+++ lms-cbt/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <p>Hello World</p>
+   <p className='text-3xl font-medium text-sky-700'>
+    Hello World
+    </p>
   )
 }
