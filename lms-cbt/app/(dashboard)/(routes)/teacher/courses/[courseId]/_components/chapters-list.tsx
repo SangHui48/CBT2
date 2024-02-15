d06e1b41b9b9daf73d78ba2fd2578c831860534a
한상희 <70365377+SangHui48@users.noreply.github.com>
2024-02-15 16:02:44 +0900

    4h 41m 03s

--- lms-cbt/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ lms-cbt/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -10,6 +10,8 @@ import {
 } from "@hello-pangea/dnd";
 
 import { cn } from "@/lib/utils";
+import { Grip } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 
 
@@ -41,8 +43,59 @@ export const ChaptersList = ({
     }
 
     return (
-        <div>
-            Chapters List
-        </div>
+        <DragDropContext onDragEnd={() => {}}>
+            <Droppable droppableId="chapters">
+                {(provided) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                        {chapters.map((chapter, index) => (
+                            <Draggable 
+                                key={chapter.id} 
+                                draggableId={chapter.id} 
+                                index={index}
+                            >
+                                {(provided) => (
+                                    <div
+                                        className={cn(
+                                            "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
+                                            chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-700"
+                                        )}
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                    >
+                                        <div
+                                            className={cn(
+                                                "px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+                                                chapter.isPublished && "border-r-sky-200 hover:bg-sky-200"
+                                            )}
+                                            {...provided.dragHandleProps}
+                                        > 
+                                            <Grip 
+                                                className="h-5 w-5"
+                                            />
+                                        </div>
+                                        {chapter.title}
+                                        <div className="ml-auto pr-2 flex items-center gap-x-2">
+                                                {chapter.isFree && (
+                                                    <Badge>
+                                                        Free
+                                                    </Badge>
+                                                )}
+                                                <Badge
+                                                    className={cn(
+                                                        "bg-slate-500",
+                                                        chapter.isPublished && "bg-sky-700"
+                                                    )}
+                                                >
+                                                    {chapter.isPublished ? "Published" : "Draft"}
+                                                </Badge>
+                                        </div>
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     )
 }
