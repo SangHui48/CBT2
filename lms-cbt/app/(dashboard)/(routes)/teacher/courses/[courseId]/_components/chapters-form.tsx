3b989364b2f3fa7cd9062e7b8b38b25012059921
SangHui48 <vodlrm123@naver.com>
2024-03-03 22:44:29 +0900

    chapter reorder functionality 다시

--- lms-cbt/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ lms-cbt/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -4,7 +4,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -82,7 +82,12 @@ export const ChaptersForm = ({
     }
 
     return (
-        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bh-slate-500/20 top-0 right-0 rounded-m flex items-center justifiy-center">
+                    <Loader2 className="animate-spin h-6 w-6 text-sky-700"/>
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Course chapters
                 <Button onClick={toggleCreating} variant="ghost">
